3a30a120d61b5d4fee09b8d2c325709f1b3f23e6
Grigory Zhukov <Grigory@zhukoff.pro>
2016-10-26 10:38:02 +0300

    Added Node.js web app with Bootstrap SB Admin 2 - Bootstrap Admin Theme template

---
+++ IoT.HomeAutomation.NodejsWebApp/routes/index.js
@@ -0,0 +1,47 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.get('/', function (reg, res) {
+    res.render('index');
+});
+router.get('/index', function (reg, res) {
+    res.render('index');
+});
+router.get('/login', function (reg, res) {
+    res.render('login');
+});
+router.get('/blank', function (reg, res) {
+    res.render('blank');
+});
+router.get('/flot', function (reg, res) {
+    res.render('flot');
+});
+router.get('/morris', function (reg, res) {
+    res.render('morris');
+});
+router.get('/grid', function (reg, res) {
+    res.render('grid');
+});
+router.get('/buttons', function (reg, res) {
+    res.render('buttons');
+});
+router.get('/icons', function (reg, res) {
+    res.render('icons');
+});
+router.get('/forms', function (reg, res) {
+    res.render('forms');
+});
+router.get('/notifications', function (reg, res) {
+    res.render('notifications');
+});
+router.get('/panels-wells', function (reg, res) {
+    res.render('panels-wells');
+});
+router.get('/tables', function (reg, res) {
+    res.render('tables');
+});
+router.get('/typography', function (reg, res) {
+    res.render('typography');
+});
+
+module.exports = router;
---
+++ IoT.HomeAutomation.NodejsWebApp/server.js
@@ -0,0 +1,19 @@
+﻿var http = require('http');
+var express = require('express');
+
+var port = Number(process.env.PORT || 3000);
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+var routes = require('./routes/index');
+
+app.use('/', routes);
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
+
+app.listen(port);
+console.log('App service is running at port ' + port);
